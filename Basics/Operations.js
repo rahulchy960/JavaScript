bd9f1b3a98e54b52ed8205c7ca4bcf274cb67c68
rahulchy960 <rahulchy960@gmail.com>
2024-12-31 20:39:31 +0530

    operations

---
+++ Basics/Operations.js
@@ -0,0 +1,22 @@
+// Primitive Operations
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2/3);
+console.log(2%3);
+console.log(2**3);
+
+// Complex Operations
+console.log(1 + "2");       // 12
+console.log("1" + 2);       // 12
+console.log(1 + 2 + "2");   // 32
+console.log("1" + 2 + 2);   // 122
+console.log(+true);         // 1     boolean to integer
+console.log(+"");           // 0     string to integer
+
+let num1, num2, num3 ;
+num1 = num2 = num3 = 5; 
+
+console.log(num1++);
+console.log(++num2);
+
