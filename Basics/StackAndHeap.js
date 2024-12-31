b68a59620bc455be5b989265edcabc53f479aafd
rahulchy960 <rahulchy960@gmail.com>
2024-12-31 23:09:07 +0530

    Stack and Heap

---
+++ Basics/StackAndHeap.js
@@ -0,0 +1,19 @@
+
+// Stack
+let Name = "Rahul";
+let newName = Name ;
+newName = "Babai";
+console.log(Name);
+console.log(newName);
+
+
+// Heap
+let myObj = {email : "rahul@gmail.com", upi : "rahul@oksbi"};
+let userOne =  myObj;
+console.log(myObj);
+console.log(userOne);
+
+userOne.email = "rahul123@gmail.com";
+console.log(myObj)
+console.log(userOne);
+
