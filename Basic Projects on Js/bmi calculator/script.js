57a0966ecbaa00fa2acbcc665d40a623d41bde5e
rahulchy960 <rahulchy960@gmail.com>
2025-01-26 03:26:38 +0530

    bmi calculator

---
+++ Basic Projects on Js/bmi calculator/script.js
@@ -0,0 +1,18 @@
+console.log("hello")
+const form = document.querySelector('form');
+form.addEventListener( 'submit', (event)=>{
+    event.preventDefault();
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const result = document.querySelector('#results');
+
+    if(height==='' || height <=0 || isNaN(height)){
+        result.innerHTML = '<span> `Please enter a valid Height ${height}`'
+    }else if(weight==='' || weight <=0 || isNaN(weight)){
+        result.innerHTML = '<span> `Please enter a valid Weight ${weight}`'
+    }else{
+        const bmi = (weight / ((height*height) / 10000)).toFixed(2);
+        result.innerHTML = `<span>Your BMI is ${bmi}</span>`
+    }
+    form.reset();
+})
