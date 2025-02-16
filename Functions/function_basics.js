d5b66c6ab1e7119646e3f3d2bdff2ac9ba107f1a
rahulchy960 <rahulchy960@gmail.com>
2025-02-16 20:41:30 +0530

    function learning

--- Functions/function_basics.js
+++ Functions/function_basics.js
@@ -2,44 +2,54 @@ function show(){
     console.log("Hello World!");
 }
 
-show // function reference
-show() // function call
+setTimeout(()=>show(),1000);
 
-function add(num1, num2){
-    return num1+num2;
-}
 
-let result = add(5,10);
-console.log(result);
+// show // function reference
+// show() // function call
 
-function userLog(user){
-    if(user === undefined){
-        return `Please Enter your name`;
-    }
-    return `${user}, Just Logged In`
-}
+// let fun = function greet(){
+//     console.log("function as a variable");
+// }
 
-console.log(userLog("Rahul"));
+// fun();
+
+// function add(num1, num2){
+//     return num1+num2;
+// }
+
+// let result = add(5,10);
+// console.log(result);
+
+// function userLog(user){
+//     if(user === undefined){
+//         return `Please Enter your name`;
+//     }
+//     return `${user}, Just Logged In`
+// }
+
+// console.log(userLog("Rahul"));
 
 // function userLog(user = "Babai"){} // default argument
 
 // function calculateCartPrice(...num){ // rest operator (accepts all the arguments and form an array)
 //     return num;
 // }
-function calculateCartPrice(val1,val2,...num){
-    return num;
-}
 
-console.log(calculateCartPrice(200,300,400,500));
+// function calculateCartPrice(val1,val2,...num){
+//     return num;
+// }
+
+// console.log(calculateCartPrice(200,300,400,500));
 
 
-function ObjPass(obj){
-    console.log(`Username is ${obj.username} and Name is ${obj.name}`);
-}
+// function ObjPass(obj){
+//     console.log(`Username is ${obj.username} and Name is ${obj.name}`);
+// }
 
-ObjPass({username:"rachchy20",name : "Rahul"});
+// ObjPass({username:"rachchy20",name : "Rahul"});
 
-function arrayAccess(Arr){
-    return Arr[0];
-}
-console.log(arrayAccess([100, 200, 300]));
+// function arrayAccess(Arr){
+//     return Arr[0];
+// }
+// console.log(arrayAccess([100, 200, 300]));
